9ca97b008cb6658d657abff11001a14b3fc26df3
Edu58 <edumuriithi58@gmail.com>
2022-05-14 13:28:14 +0300

    adds base.html and index.html

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,19 @@
 from flask import Flask
 from config import config_option
+from flask_bootstrap import Bootstrap
+from flask_moment import Moment
+
+
+bootstrap = Bootstrap()
+moment = Moment()
 
 
 def create_app(config_name):
     app = Flask(__name__)
     app.config.from_object(config_option[config_name])
+
+    bootstrap.init_app(app)
+    moment.init_app(app)
 
     from app.main import main
     app.register_blueprint(main)
