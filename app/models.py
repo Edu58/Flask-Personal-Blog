a4d187ac476dbb543cc001f94e92212fe2f4663e
Edu58 <edumuriithi58@gmail.com>
2022-05-14 16:23:29 +0300

    feat: adds User model

--- app/models.py
+++ app/models.py
@@ -1,5 +1,27 @@
 from . import db
 from datetime import datetime
+from werkzeug.security import generate_password_hash, check_password_hash
+
+
+class User(UserMixin, db.Model):
+    __tablename__ = 'authors'
+
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(100), nullable=False)
+    first_name = db.Column(db.String(25), nullable=False)
+    last_name = db.Column(db.String(25), nullable=False)
+    pass_secure = db.Column(db.String(), nullable=False)
+
+    @property
+    def password(self):
+        return AttributeError('Password cannot be read')
+
+    @password.setter
+    def password(self, password):
+        self.pass_secure = generate_password_hash(password, method='sha256', salt_length=8)
+
+    def verify_password(self, password):
+        return check_password_hash(self.pass_hashed, password)
 
 
 class BlogPost(db.Model):
