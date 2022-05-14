fac4c3011ae57bf17d30a173d2f76e1fe8c7e7eb
Edu58 <edumuriithi58@gmail.com>
2022-05-14 14:46:01 +0300

    feat: shows all blogposts

--- app/main/views.py
+++ app/main/views.py
@@ -1,7 +1,9 @@
 from . import main
+from app.models import BlogPost
 from flask import render_template
 
 
 @main.route('/')
 def index():
-    return render_template('index.html')
+    blogposts = BlogPost.query.all()
+    return render_template('index.html', blogposts=blogposts)
