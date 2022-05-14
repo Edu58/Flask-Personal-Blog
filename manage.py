045c7af5fa34509ff892067ebdab221c791ae478
Edu58 <edumuriithi58@gmail.com>
2022-05-14 14:26:10 +0300

    first migration

--- manage.py
+++ manage.py
@@ -1,6 +1,7 @@
 from app import create_app, db
 from flask_script import Server, Manager
 from flask_migrate import Migrate, MigrateCommand
+from app.models import BlogPost
 
 app = create_app('development')
 manager = Manager(app)
@@ -11,7 +12,7 @@ manager.add_command('db', MigrateCommand)
 
 @manager.shell
 def make_shell_context():
-    return dict()
+    return dict(db=db, bp=BlogPost)
 
 
 @manager.command
